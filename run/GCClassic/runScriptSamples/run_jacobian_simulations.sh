7574185ce23e3a40ae6cf7c637de1973e16a6436
djvaron <danieljordanvaron@gmail.com>
2021-06-15 09:32:33 -0400

    Update run_jacobian_simulations.sh
    - Removed feature where multiple runs are submitted per slurm job. 
    - Now one run per job

--- run/GCClassic/runScriptSamples/run_jacobian_simulations.sh
+++ run/GCClassic/runScriptSamples/run_jacobian_simulations.sh
@@ -13,27 +13,19 @@ RUNDIR=$(pwd -P)
 ### Get current task ID
 x=${SLURM_ARRAY_TASK_ID}
 
-### The task is going to run a series of 4 jobs. For each job:
-for iter in {0..3}; do
+### Add zeros to the cluster Id
+if [ $x -lt 10 ]; then
+    xstr="000${x}"
+elif [ $x -lt 100 ]; then
+    xstr="00${x}"
+elif [ $x -lt 1000 ]; then
+    xstr="0${x}"
+else
+    xstr="${x}"
+fi
 
-  ### Select a cluster Id
-  xi=$((4*x+iter))
-
-  ### Add zeros to the cluster Id
-  if [ $xi -lt 10 ]; then
-      xstr="000${xi}"
-  elif [ $xi -lt 100 ]; then
-      xstr="00${xi}"
-  elif [ $xi -lt 1000 ]; then
-      xstr="0${xi}"
-  else
-      xstr="${xi}"
-  fi
-
-   ### Run GEOS-Chem in the directory corresponding to the cluster Id
-  cd  ${RUNDIR}/{RunName}_${xstr}
-  ./{RunName}_${xstr}.run
-
-done
+### Run GEOS-Chem in the directory corresponding to the cluster Id
+cd  ${RUNDIR}/{RunName}_${xstr}
+./{RunName}_${xstr}.run
 
 exit 0
